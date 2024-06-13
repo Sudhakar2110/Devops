9365a5569580a4acd207fefc381e9b65c570b547
Sudhakar21101 <sudhakar.p6@cognizant.com>
2024-06-13 15:41:40 +0530

    towar

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,14 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
-public class App {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class App extends HttpServlet{
 
     private static final String MESSAGE = "Hello World!";
 
@@ -15,5 +20,12 @@ public class App {
 
     public String getMessage() {
         return MESSAGE;
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<html><body>");
+        response.getWriter().println("<h2>Hello"+getMessage() +"</h2>");
+        response.getWriter().println("</body></html>");
     }
 }
